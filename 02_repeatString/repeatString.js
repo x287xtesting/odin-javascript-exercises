5270b43783f4bff480bc00ccdddf83e5ede79ec6
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-18 18:05:09 +0300

    Solve repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0)
+        return "ERROR";
+    num = num ?? Math.floor(Math.random() * 1000);
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result = result + string;
+    }
+    return result;
 };
 
 // Do not edit below this line
