5353735490a9eda0020ab1169960374d19f0287a
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-18 17:30:30 +0300

    Comment about how Jest works in helloWorld.spec.js

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,20 @@
 const helloWorld = require('./helloWorld');
 
-describe('Hello World', function() {
-  test('says "Hello, World!"', function() {
+describe('Hello World', function () {
+  test('says "Hello, World!"', function () {
     expect(helloWorld()).toEqual('Hello, World!');
   });
 });
+
+/**
+ * Basically in Jest test file you write a describe function
+ * which takes the name of the function, and another function as parameters
+ * you put your tests in the other function,
+ * those tests test the function you've described in the describe function
+ * 
+ * describe('Name of the function', function() {
+ *  test('description of the test', function(){
+ *    expect(yourFunction().toEqual(something);
+ *  });
+ * });
+ */
