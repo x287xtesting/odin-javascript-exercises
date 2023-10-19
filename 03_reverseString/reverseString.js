f65005937c33cb623219d2d5fc2a4ff053f29c22
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-19 09:52:18 +0300

    Solve reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+/**
+ * 
+ * @param {string} string 
+ * @returns {string}
+ */
+const reverseString = function (string) {
+    if (string === "" || string === undefined) {
+        return "";
+    }
 
+    let reversed = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
