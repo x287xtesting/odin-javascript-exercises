b6c69450bdee0f3effc525f798f49667dc17875a
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-19 10:26:15 +0300

    Solve sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
