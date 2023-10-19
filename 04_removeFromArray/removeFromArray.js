4b100a815b50e06fc321eca9ec897b4d11b463b4
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-19 10:07:23 +0300

    Solve removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+/**
+ * 
+ * @param {[any]} arr 
+ * @param  {...any} items 
+ */
+const removeFromArray = function (arr, ...items) {
+    for (let item of items) {
+        if (arr.includes(item)) {
+            arr = arr.filter(e => e != item);
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
