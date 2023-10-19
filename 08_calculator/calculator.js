86f0fc8125571214bb4acea4b527364e45696f62
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-19 12:06:43 +0300

    Solve calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+/**
+ * 
+ * @param {[number]} arr 
+ */
+const sum = function (arr) {
+  return arr.reduce((e1, e2) => e1 + e2, 0);
 };
 
-const multiply = function() {
 
+/**
+ * 
+ * @param {[number]} arr 
+ */
+const multiply = function (arr) {
+  return arr.reduce((e1, e2) => e1 * e2);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
