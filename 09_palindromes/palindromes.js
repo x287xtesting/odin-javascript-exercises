3f1d9446c241b5e5a924e7347129ebd62d455f6b
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-19 12:27:24 +0300

    Solve palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+/**
+ * 
+ * @param {string} string 
+ * @returns {boolean} - Whether the string is a palindrome or not
+ */
+const palindromes = function (string) {
+    let chars = string.split("").filter(char => char.match(/[a-z\d]/i)).map(char => char.toLowerCase());
+    console.log(chars);
+    for (let i = 0; i <= Math.floor(chars.length / 2); i++) {
+        if (chars[i] !== chars[chars.length - 1 - i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
