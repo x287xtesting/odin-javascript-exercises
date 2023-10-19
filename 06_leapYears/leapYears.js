25cc62291a39c19f79406cbf090edefc913edb89
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-19 10:44:23 +0300

    Solve leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+/**
+ * 
+ * @param {number} year 
+ * @returns {boolean}
+ */
+const leapYears = function (year) {
+    if (year % 100 == 0 && year % 400 == 0) return true;
+    if (year % 100 != 0 && year % 4 == 0) return true;
+    return false;
 };
 
 // Do not edit below this line
