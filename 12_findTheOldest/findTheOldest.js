bcfcb5a0729138f681ccc827c1a7988028682e96
x287xtesting <115352423+x287xtesting@users.noreply.github.com>
2023-10-20 11:07:46 +0300

    Solve findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+/**
+ * 
+ * @param {Array<{name: string, yearOfBirth: number, yearOfDeath?: number}>} people - An array of people
+ * @returns {{name: string, yearOfBirth: number, yearOfDeath?: number}} The oldest person in the people array
+ */
+const findTheOldest = function (people) {
+    return people.reduce((person1, person2) => {
+        return findAge(person1) > findAge(person2) ? person1 : person2;
+    })
 
-};
+    function findAge(person) {
+        let { yearOfBirth, yearOfDeath } = person;
+        yearOfDeath = yearOfDeath ?? new Date().getFullYear();
+        return yearOfDeath - yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
